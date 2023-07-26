2a45df0c0c7f56c94344a5bcb1899933d4433a02
MarcoBonillaY <marcobonilla921@gmail.com>
2023-07-26 04:49:20 -0500

    todo

--- Views/template/header.php
+++ Views/template/header.php
@@ -115,7 +115,7 @@
 
                     </div>
                 </div>
-                <div class="sb-sidenav-footer bg-primary #CC6D00">
+                <div class="sb-sidenav-footer bg-primary #CC0000">
                     <div class="small">Sígueme: </div>
                     <a href="https://www.facebook.com/profile.php?id=100072379307819" class="text-white"><i class="fab fa-facebook-square"></i> Facebook</a>
                     <a href="https://www.instagram.com/yepez_said/" class="text-danger"><i class="fab fa-instagram-square"></i> Instagram</a>
